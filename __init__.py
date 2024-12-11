4191c6974e018adf8b110078005fa317fc29f4ba
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2024-12-11 22:27:22 +0530

    feat: create partition helper (#75)
    * feat: create partition helper
    
    * feat: partitioning
    
    * feat: partitioning
    
    * feat: partitioning
    
    * feat: partition by month and quarter
    
    * feat: partition by field
    
    * feat: partition for child doctypes
    
    * feat: partition for child doctypes
    
    * wip: add partition helper script to pyproject.toml
    
    * wip: add create partition import
    
    * wip: v13 meta._fields fix
    
    * fix: skip standard fields
    
    * wip: restore partitions
    
    * wip: rm debug
    
    * wip: improve get_partitions_to_backup
    
    * feat: bkp and restore for partitions in the same machine
    
    * feat: compress and path
    
    * feat: option to exclude non partitioned tables
    
    * feat: option to exclude non partitioned tables
    
    * fix: add_custom_field
    
    * frix: csv file size limit (#83)
    
    ---------
    
    Co-authored-by: Francisco Roldan <franciscoproldan@gmail.com>
    Co-authored-by: Tyler Matteson <tyler@agritheory.com>

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 from test_utils.utils.conftest import db_instance, monkeymodule
 from test_utils.utils.customize import add_customization_hash, load_customizations
+from test_utils.utils.create_partition import create_partition
 
 try:
 	__version__ = version("test_utils")
